0983e83ebdce7b07ec067f56a783b6ad78418f85
brian-insua <brian.insua@gmail.com>
2016-04-28 14:44:43 +0100

    Update main.c

--- Frame_Processor_Assignment/main.c
+++ Frame_Processor_Assignment/main.c
@@ -2,6 +2,12 @@
 //  main.c
 //  Frame_Processor_Assignment
 //
+// Instructions:
+//
+// To use in command line compile with:
+//
+// "clang Frame_Processor_Assignment.c -l SDL2 -o Frame_Processor_Assignment"
+//
 //  Created by Brian Insua on 26/04/2016.
 //  Copyright © 2016 Brian Insua. All rights reserved.
 //
@@ -9,9 +15,15 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
+
+
 int main(int argc, const char * argv[])
 {
-    printf("Frame Processor Assignment\n"); /* Following from SDL Wiki & Eike */
+    printf("Frame Processor Assignment\n"); /* Following from SDL Wiki, Gigi.NullNeuron.net & Eike */
+    
+    int done = 0; /* E - Starting State set to 0 to keep window open */
+    
+    SDL_Event ShowFrame; /* G - Declare Opening and Running window an event */
     
     SDL_Window *window; /* W - Declare Pointer */
     
@@ -19,15 +31,15 @@ int main(int argc, const char * argv[])
     
     window = SDL_CreateWindow("Frame Prcessor Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN); /* W - Creates Window */
     
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) /* E - checks if SDL2 initialised */
     {
         printf("Error: %s \n", SDL_GetError());
         return 1;
-    } /* E - checks if SDL2 initialised */
+    } 
     
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC); /* E - Create Renderer (Frame Buffer) */
     
-    SDL_Surface *pic = SDL_LoadBMP("ThisPicture.bmp"); /* E - Loads Bitmap Image */
+    SDL_Surface *pic = SDL_LoadBMP("AnotherPicture.bmp"); /* E - Loads Bitmap Image */
     
     SDL_Texture *tex = SDL_CreateTextureFromSurface(renderer, pic); /* E - Converts Texture to Surface */
     
@@ -37,9 +49,23 @@ int main(int argc, const char * argv[])
     
     SDL_RenderPresent(renderer); /* W - Shows Render */
     
-    SDL_Delay(10000); /* W - Pauses execution for # of miliseconds */
+    SDL_Delay(1000); /* W - Pauses execution for # of miliseconds */
     
-    SDL_DestroyTexture(tex); /* Clears Memory */
+	while( ! done ) /* G & E -  While loop to keep window open until user clicks close window option */
+    {
+		while (SDL_PollEvent(&ShowFrame))
+		{
+			SDL_WaitEvent(&ShowFrame);
+			switch (ShowFrame.type)
+			{
+				case SDL_QUIT:
+					done = 1;
+					break;
+			}
+		}
+	}
+    
+    SDL_DestroyTexture(tex); /* E - Clears Memory */
     
     SDL_DestroyWindow(window); /* W - Closes Window */
     
